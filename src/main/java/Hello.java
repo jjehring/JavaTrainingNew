cf629b62cb4187eaca304ef6fe02e00e9c21db1d
jjehring <joerg.jehring@vodafone.com>
2020-03-02 13:32:50 +0100

    initial

---
+++ src/main/java/Hello.java
@@ -0,0 +1,13 @@
+//import interface
+public class Hello {
+    public static void main (String [] args) {
+        System.out.println("Hello Test again");
+
+        System.out.println("Test nextswwwwwss ");
+        for (int i = 0; i <= 50; i++) {
+            System.out.println( "Test: " + i );
+        }
+    }
+}
+
+
---
+++ src/main/java/Helloneu.java
@@ -0,0 +1,12 @@
+//import interface
+public class Helloneu {
+    public static void main (String [] args) {
+        System.out.println("Hello Test again");
+        System.out.println("Test nextswwwwwss ");
+        for (int i = 0; i <= 5000; i++) {
+            System.out.println( "Test: " + i );
+        }
+    }
+}
+
+
