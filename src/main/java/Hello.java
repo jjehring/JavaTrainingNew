7cd001e1f731c203997604a59e4c6e0d72cfb300
jjehring <joerg.jehring@vodafone.com>
2020-03-02 13:43:54 +0100

    my first objectz

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,12 +1,28 @@
-//import interface
+import java.util.Random;
 public class Hello {
     public static void main (String [] args) {
         System.out.println("Hello Test again");
 
         System.out.println("Test nextswwwwwss ");
-        for (int i = 0; i <= 50; i++) {
+        for (int i = 0; i <= 5; i++) {
             System.out.println( "Test: " + i );
+            if (i > 10 && i < 20) {
+                System.out.println("bin im If von Thorsten" + i);
+            }
         }
+
+        System.out.println(2 + 5);
+        System.out.println(5-3);
+        System.out.println(5*3);
+        System.out.println(5/2);
+
+        int average = (3+4+5)/3;
+        System.out.println("meine erste VAr: " + average  );
+
+        Random r = new Random();
+        System.out.println(r.nextInt(1000));
+
+
     }
 }
 
